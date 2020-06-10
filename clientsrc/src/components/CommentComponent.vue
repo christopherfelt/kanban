b5e60044433c6ff0bbfbeaca63ad430b1e1f23a8
chris_MSI1 <chris.h.felt@gmail.com>
2020-06-09 20:12:58 -0600

    added comment component

---
+++ clientsrc/src/components/CommentComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <li class="comment">
+    <small>{{ comment.body }}</small>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "Comment",
+  data() {
+    return {};
+  },
+  props: ["comment"],
+  computed: {},
+  methods: {},
+  components: {},
+};
+</script>
+
+<style scoped></style>
--- clientsrc/src/components/TaskComponent.vue
+++ clientsrc/src/components/TaskComponent.vue
@@ -1,19 +1,38 @@
 <template>
   <li class="task d-flex justify-content-center list-group-item ">
     {{ task.body }}
+
+    <ul>
+      <comment
+        v-for="comment in activeComments"
+        :key="comment.id"
+        :comment="comment"
+      />
+    </ul>
   </li>
 </template>
 
 <script>
+import Comment from "./CommentComponent.vue";
+
 export default {
   name: "Task",
+  mounted() {
+    this.$store.dispatch("getCommentsByTaskId", this.task.id);
+  },
   data() {
     return {};
   },
   props: ["task"],
-  computed: {},
+  computed: {
+    activeComments() {
+      return this.$store.state.activeComments[this.task.id];
+    },
+  },
   methods: {},
-  components: {},
+  components: {
+    Comment,
+  },
 };
 </script>
 
